b1ee2d16d6150628ebb1b8a69317cb3976cce047
Mirko Brombin <send@mirko.pm>
2021-03-11 18:09:02 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='libwine',
+    version='0.1',
+    license='GPL-3.0',
+    description='A python library for interacting with Wine.',
+    keywords='libwine wine proton windows',
+
+    url='https://github.com/bottlesdevs/libwine',
+
+    author='Mirko Brombin',
+    author_email='send@mirko.pm',
+
+    packages=find_packages()
+)
