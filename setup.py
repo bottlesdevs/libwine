21c2eccc95017f181e77f1662d1ab2cea7410a3a
Mirko Brombin <send@mirko.pm>
2021-04-05 10:09:50 +0200

    Release 0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libwine',
-    version='0.3',
+    version='0.4',
     license='GPL-3.0',
     description='A python library for interacting with Wine.',
     keywords='libwine wine proton windows',
